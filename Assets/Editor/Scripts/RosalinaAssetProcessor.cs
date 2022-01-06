3867701ac2128d7dda1bb4f82467b010c98d74db
Filipe GP <4021025+Eastrall@users.noreply.github.com>
2022-01-06 21:26:41 +0100

    Add basic unity solution

---
+++ Assets/Editor/Scripts/RosalinaAssetProcessor.cs
@@ -0,0 +1,25 @@
+using Microsoft.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class RosalinaAssetProcessor : AssetPostprocessor
+{
+    private const string UIDocumentExtension = ".uxml";
+
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromPath)
+    {
+        string[] uiFilesChanged = importedAssets.Where(x => Path.GetExtension(x) == UIDocumentExtension).ToArray();
+
+        if (uiFilesChanged.Length > 0)
+        {
+            var generator = new RosalinaGenerator();
+
+            for (int i = 0; i < uiFilesChanged.Length; i++)
+            {
+                generator.Generate(uiFilesChanged[i]);
+            }
+        }
+    }
+}
---
+++ Assets/Editor/Scripts/RosalinaGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.IO;
+using UnityEditor;
+
+internal class RosalinaGenerator
+{
+    /// <summary>
+    /// Generates the UI document code behind.
+    /// </summary>
+    /// <param name="uiDocumentPath">UI Document path.</param>
+    public void Generate(string uiDocumentPath)
+    {
+        EditorUtility.DisplayProgressBar("Generating UI code behind", "Working...", 0);
+        string outputPath = Path.GetDirectoryName(uiDocumentPath);
+        string className = Path.GetFileNameWithoutExtension(uiDocumentPath);
+        string generatedCodeBehindFileName = Path.Combine(outputPath, $"{className}.g.cs");
+
+        ClassDeclarationSyntax @class = SyntaxFactory.ClassDeclaration(className)
+            .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+            .AddModifiers(SyntaxFactory.Token(SyntaxKind.PartialKeyword));
+
+        string code = @class
+            .NormalizeWhitespace()
+            .ToFullString();
+
+        File.WriteAllText(generatedCodeBehindFileName, code);
+
+        EditorUtility.ClearProgressBar();
+    }
+}
