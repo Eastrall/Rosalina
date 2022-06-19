1d1a1c1709623640af62a5ae71ac8292ef72e7c7
Philipp Sigloch <sigloch.philipp@gmail.com>
2022-06-19 13:14:02 +0200

    refs #16 - fixes typo leading to wring behaviour

--- Editor/Scripts/RosalinaAssetProcessor.cs
+++ Editor/Scripts/RosalinaAssetProcessor.cs
@@ -12,7 +12,7 @@ public class RosalinaAssetProcessor : AssetPostprocessor
     private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromPath)
     {
         string[] uiFilesChanged = importedAssets
-            .Where(x => x.StartsWith("Asset/"))
+            .Where(x => x.StartsWith("Assets"))
             .Where(x => Path.GetExtension(x) == UIDocumentExtension)
             .ToArray();
 
